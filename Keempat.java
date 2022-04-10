aa37cfa9f5f3c8575ff9598de0921fe1c44884f4
David Sinaga <davidsinaga99d@gmail.com>
2022-04-11 00:46:35 +0700

    Add files via upload

---
+++ Keempat.java
@@ -0,0 +1,53 @@
+public class Keempat{
+
+public static void main(String[] args){
+
+    //operasi aritmatika
+
+    int variable1 = 876;
+    int variable2 = 666;
+
+    int hasil;
+     
+    //penjumlahan cok
+
+    hasil = variable1 + variable2;
+    System.out.println(variable1 + " + " + variable2 + " = " + hasil);
+
+    //pengurangan cok
+
+    hasil = variable1 - variable2;
+    System.out.println(variable1 + " - " + variable2 + " = " + hasil);
+
+    //perkalian cok
+
+    hasil = variable1 * variable2;
+    System.out.println(variable1 + " x " + variable2 + " = " + hasil);
+
+    //pembagian cok
+
+    hasil = variable1 / variable2;
+    System.out.println(variable1 + " / " + variable2 + " = " + hasil);
+
+    float d = 300;
+    float e = 87;
+    float hasilFloat = d/e;
+    System.out.println("d " + " / " + " e " + " = " + hasilFloat);
+
+    double a = 875.654;
+    double b = 762.087;
+    double hasilDouble = a/b;
+    System.out.println("a " + " / " + " b " + " = " + hasilDouble);
+
+    //modulus cok
+
+    hasil = variable1 % variable2;
+    System.out.println(hasil);
+
+    //hasiil pengurangan cok
+
+    hasil = variable1 % variable2;
+    System.out.println(hasil);
+    
+}
+}
---
+++ Ketiga.java
@@ -0,0 +1,76 @@
+public class Ketiga {
+
+    public static void main(String[] args){
+        System.out.println("Saya David Sinaga");
+
+        //mau bikin integer
+        int a = 5;
+        System.out.println("=======INTEGER=======");
+        System.out.println("nilai integer " + (a + 2));
+        System.out.println("nilai max = " + Integer.MAX_VALUE);
+        System.out.println("nilai min = " + Integer.MIN_VALUE);
+        System.out.println("bytes nya integer = " + Integer.BYTES + " bytes");
+        System.out.println("bit nya integer = " + Integer.SIZE + " bit");
+
+        //mau bikin byte
+        byte b = 5;
+        System.out.println("=======BYTE=======");
+        System.out.println("nilai byte " + (b + 2));
+        System.out.println("nilai max = " + Byte.MAX_VALUE);
+        System.out.println("nilai min = " + Byte.MIN_VALUE);
+        System.out.println("bytes nya byte = " + Byte.BYTES + " bytes");
+        System.out.println("bit nya byte = " + Byte.SIZE + " bit");
+
+        //mau bikin long
+        long c = 5L;
+        System.out.println("=======LONG=======");
+        System.out.println("nilai long " + (c + 2));
+        System.out.println("nilai max = " + Long.MAX_VALUE);
+        System.out.println("nilai min = " + Long.MIN_VALUE);
+        System.out.println("bytes nya long = " + Long.BYTES + " bytes");
+        System.out.println("bit nya long = " + Long.SIZE + " bit");
+
+        //mau bikin short
+        short d = 5;
+        System.out.println("=======SHORT=======");
+        System.out.println("nilai short " + (d + 2));
+        System.out.println("nilai max = " + Short.MAX_VALUE);
+        System.out.println("nilai min = " + Short.MIN_VALUE);
+        System.out.println("bytes nya short = " + Short.BYTES + " bytes");
+        System.out.println("bit nya short = " + Short.SIZE + " bit");
+
+        //mau bikin float
+        float e = 8.99f;
+        System.out.println("=======FLOAT=======");
+        System.out.println("nilai float " + (e + 2));
+        System.out.println("nilai max = " + Float.MAX_VALUE);
+        System.out.println("nilai min = " + Float.MIN_VALUE);
+        System.out.println("bytes nya float = " + Float.BYTES + " bytes");
+        System.out.println("bit nya float = " + Float.SIZE + " bit");
+ 
+        //mau bikin double
+        double f = 5.123456789d;
+        System.out.println("=======DOUBLE=======");
+        System.out.println("nilai double " + (f + 2));
+        System.out.println("nilai max = " + Double.MAX_VALUE);
+        System.out.println("nilai min = " + Double.MIN_VALUE);
+        System.out.println("bytes nya double = " + Double.BYTES + " bytes");
+        System.out.println("bit nya double = " + Double.SIZE + " bit");
+
+      //mau bikin character
+      char g = 'y';
+      System.out.println("=======CHARACTER=======");
+      System.out.println("nilai char adalah " + g);
+      System.out.println("nilai max = " + Character.MAX_VALUE);
+      System.out.println("nilai min = " + Character.MIN_VALUE);
+      System.out.println("bytes nya char = " + Character.BYTES + " bytes");
+      System.out.println("bit nya char = " + Character.SIZE + " bit"); 
+
+      //mau bikin boolean
+      boolean val = false;
+      System.out.println("=======BOOLEAN=======");
+      System.out.println("nilai char adalah " + val);
+      System.out.println("bytes nya char = " + Boolean.TRUE);
+      System.out.println("bit nya char = " + Boolean.FALSE);
+    } 
+}
