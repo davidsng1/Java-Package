9830d8dd0cc50223b3a9180b221074e8fada94e9
David Sinaga <davidsinaga99d@gmail.com>
2022-04-11 00:27:37 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    
+    public static void main(String[] args){
+        System.out.println("david");
+        System.out.println("Hello World");
+        System.out.println("saya " + args [0] + " Tapi boong");
+    }
+}
